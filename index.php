2f38300a6702f36e97416d57651ab5cf3a952981
Pablo Ceballos <pablo-s_97@hotmail.com>
2015-10-16 20:01:46 -0500

    Primer Commit creacion del Index y carpetas
    cree las carpetas para tener orden y asi facilitaremos el trabajo en
    grupo, cree el index con link ya creados

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Ficha 893311</title>
+  </head>
+  <body>
+    <h1>Registro Ficha 893311</h1>
+    <div class="links">
+      <a href="modificar/modificar.php">Modificar Aprendiz</a>
+      <a href="eliminar/eliminar.php">Eliminar Aprendiz</a>
+      <a href="consultar/consultar.php">Consultar Aprendiz</a>
+      <a href="registrar/registrar.php">Registrar Aprendiz</a>
+    </div>
+    <footer>
+      <h4>©EcapSoft 2015</h4>
+    </footer>
+  </body>
+</html>
